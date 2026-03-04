8e41d9c7dd55c027f7fc9c7094ff1f5fdbe25f64
ZzBeNnzZ <benle030204@gmail.com>
2026-03-03 23:10:45 -0600

    Added analytics to track page visits on Vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import EmailPopup from "@/components/EmailPopup";
+import { Analytics } from "@vercel/analytics/next";
 
 const playfair = Playfair_Display({
   variable: "--font-playfair",
@@ -34,6 +35,7 @@ export default function RootLayout({
         <main>{children}</main>
         <Footer />
         <EmailPopup />
+        <Analytics />
       </body>
     </html>
   );
